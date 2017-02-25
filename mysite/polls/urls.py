e6b46c617dfbd1c0a33e878f62c509a0823d341a
matthew goodman <mattsgood2@aol.com>
2017-02-25 23:18:47 +0000

    template stuff, voting redirect

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 
 from . import views
-#added name to the urls to namespace if more apps like below
+# added name to the urls to namespace if more apps like below
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
