cd802a74b9c79fd3ac807cdf0c28bd95c83de58b
matthew goodman <mattsgood2@aol.com>
2017-02-25 15:28:13 +0000

    added templates and name spaced urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 
 from . import views
-
+#added name to the urls to namespace if more apps like below
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<question_id>[0-9]+)/votes/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
