d7030119950601ecaeefb503e2f1b4c9fe377c6c
matthew goodman <mattsgood2@aol.com>
2017-03-19 19:29:33 +0000

    added admin fields for better view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+#added this to show up in admin for better view
+#the class must go above the admin.site.reg to work
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date',)
+
+
+admin.site.register(Question, QuestionAdmin)
+# added Choice so you can see whats there
+
+#added this to show up in admin for better view
+#the class must go above the admin.site.reg to work
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+
+admin.site.register(Choice, ChoiceAdmin)
